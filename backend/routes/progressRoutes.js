abbb1554c98801e0b29e46c49305c4623d4fa118
Orni Rodrigues <ornimathew2002@gmail.com>
2026-05-07 22:41:22 +0600

    added

--- backend/routes/progressRoutes.js
+++ backend/routes/progressRoutes.js
@@ -3,6 +3,7 @@ const protect = require("../middlewares/authMiddleware");
 
 const {
   getMyProgressLogs,
+  getAllProgressLogs,
   createProgressLog,
   updateProgressLog,
   deleteProgressLog,
@@ -11,6 +12,10 @@ const {
 const router = express.Router();
 
 router.get("/mine", protect, getMyProgressLogs);
+router.get("/my-progress", protect, getMyProgressLogs);
+
+router.get("/admin/all", protect, getAllProgressLogs);
+
 router.post("/", protect, createProgressLog);
 router.put("/:id", protect, updateProgressLog);
 router.delete("/:id", protect, deleteProgressLog);
